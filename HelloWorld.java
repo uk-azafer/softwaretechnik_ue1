13bdf4d218d8481b4a41cb79a3250ff8a5077126
Alisan Zafer <zafer.alisan@gmail.com>
2020-11-03 21:19:06 +0100

    Commite 1. Änderung entprechend Aufgabe 3 UE1

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ package softwaretechnik_ue1;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Alisan Zafer");
     }
 }
